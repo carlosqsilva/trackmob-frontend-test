1f239df5b4af46900ea29a6cccb8b4fa1dea6619
Carlos Silva <carlosqsilva@outlook.com>
2018-01-24 22:17:43 -0300

    refactor cpf validator

--- src/Components/Form/cpf.js
+++ src/Components/Form/cpf.js
@@ -1,52 +1,29 @@
+const checker = (str) => {
+  const num = str.length + 1  
+  let sum = str.split('').reduce((acumulator, value, index) => {
+    return acumulator + value * (num-index)
+  }, 0)
+  const check = sum % 11 
+  return (check < 2) ? 0: 11 - check
+}
 
-export const validarCPF = (value) => {
-  const str = value.replace(/\.|-|\s/g, '')
-
+export const validarCPF = (values) => {
+  const str = values.replace(/\.|-|\s/g, '')
+  // cpf deve conter 11 caracteres
   if (str.length !== 11) {
     return false
   }
-
-  const nine = str.substring(0,9) // Primeiros 9 digitos
-  const verificador = str.substring(9,11) // Ultimos 2 digitos
-
-  // Os digitos são iguais?
-  for (let i = 0; i < 10; i++) {
-    if (str === Array(12).join(i)) {
-      return false
-    }
+  // são todos iguais?
+  if (str.split('').every( v => v === str[0] )) {
+    return false
   }
-
-  const v1 = calcChecker1(nine)
-  const v2 = calcChecker2(`${nine}${v1}`)
-
-  if (verificador.toString() === v1.toString() + v2.toString()) {
-    return true
+  
+  const v1 = checker(str.substring(0,9)) // primeiro digito verificador
+  const v2 = checker(str.substring(0,9) + v1.toString()) // segundo digito verificador
+  
+  // valor calculado é igual ao fornecido?
+  if (str.substring(9,11) === v1.toString() + v2.toString()) {
+    return false
   }
-  return false
-}
-
-function calcChecker1 (str) {
-  let sum = null
-
-  for (let j = 0; j < 9; ++j) {
-    sum += str.toString().charAt(j) * (10 - j)
-  }
-
-  const verificador = sum % 11
-  const v1 = (verificador < 2) ? 0 : 11 - verificador
-
-  return v1
-}
-
-function calcChecker2 (str) {
-  let sum = null
-
-  for (let k = 0; k < 10; ++k) {
-    sum += str.toString().charAt(k) * (11 - k)
-  }
-
-  const verificador = sum % 11
-  const v2 = (verificador < 2) ? 0 : 11 - verificador
-
-  return v2
+  return false    
 }
